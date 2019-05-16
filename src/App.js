d42e07cfccd79c1bbce74ae75de25d0dff2e652c
161250056 <161250056@smail.nju.edu.cn>
2019-05-16 19:46:00 +0800

    修改样式为侧边栏，重新配置router

--- src/App.js
+++ src/App.js
@@ -1,35 +1,89 @@
 import React, { Component } from 'react';
-import {Button, Layout} from 'antd';
-import { BrowserRouter, Route } from 'react-router-dom';
+import 'antd/dist/antd.css';
+import { Layout, Menu, Icon, Row, Col} from 'antd';
+import {Route, Switch, Link, BrowserRouter} from 'react-router-dom'
 import HeaderNav from './common/header';
 import Comments from './pages/comments';
 import Detail from './pages/detail';
-import KPicture from './pages/kPicture'
+import KPicture from './pages/kPicture';
 
-const { Header, Footer, Content } = Layout;
+require ('./App.css');
+
+const { Header, Sider, Content } = Layout;
 
 class App extends Component{
+    state = {
+        collapsed: false,
+    };
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+
   render(){
     return (
-            <div>
+        <BrowserRouter>
+            <Layout style={{height: '100%', position: 'absolute', width: '100%'}}>
+                <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+                    <div id='logo'/>
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+                        <Menu.Item key="1">
+                            <Link to="/">
+                                <Icon type="database" />
+                                <span>信息对比</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Link to="/comments">
+                                <Icon type="team" />
+                                <span>评论</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Link to="/kPicture">
+                                <Icon type="line-chart" />
+                                <span>k线图</span>
+                            </Link>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
                 <Layout>
-                    <Header>
-                        <HeaderNav/>
+                    <Header style={{ background: '#fff', padding: 0 }}>
+                        <Row>
+                            <Col span={8}>
+                                <Icon
+                                    id='trigger'
+                                    type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                                    onClick={this.toggle}
+                                />
+                            </Col>
+                            <Col span={8}>
+                                <HeaderNav />
+                            </Col>
+                        </Row>
                     </Header>
-                    <Content style={{marginTop: '80px'}}>
-                        <BrowserRouter>
-                            <div>
+                    <Content
+                        style={{
+                            margin: '24px 16px',
+                            padding: 24,
+                            background: '#fff',
+                            minHeight: 280,
+                        }}
+                    >
+                        <Route>
+                            <Switch>
                                 <Route path='/' exact component={Detail}/>
                                 <Route path='/comments' exact component={Comments}/>
                                 <Route path='/kPicture' exact component={KPicture}/>
-                            </div>
-                        </BrowserRouter>
+                            </Switch>
+                        </Route>
+
                     </Content>
-                    <Footer>
-                        皮卡皮卡~
-                    </Footer>
                 </Layout>
-            </div>
+            </Layout>
+        </BrowserRouter>
 
     );
   }
